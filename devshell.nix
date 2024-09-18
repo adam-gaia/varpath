0c26d5a3594d4c4fde78e537d02f640785e8ac89
Adam Gaia <git@agaia.dev>
2024-09-18 14:55:40 -0600

    build: add blueprint for nix flake

---
+++ devshell.nix
@@ -0,0 +1,42 @@
+{
+  inputs,
+  pkgs,
+  perSystem,
+}:
+let
+  craneLib = inputs.crane.mkLib pkgs; # TODO: this is defined both here and in package.nix. Can we share it in a module?
+
+  # Treefmt doesn't easily expose the programs with out its flake-parts module (as far as I can tell)
+  # This snipit, modified from their default.nix, lets us grab the programs after buiding with our treefmt config
+#  treefmt-module-builder = nixpkgs: configuration: let
+#    mod = treefmt-nix.lib.evalModule nixpkgs configuration;
+#  in
+#    mod.config.build;
+
+#  treefmt-module = treefmt-module-builder pkgs (import ./treefmt.nix);
+#  treefmt-bin = treefmt-module.wrapper;
+#  treefmt-programs = lib.attrValues treefmt-module.programs;
+
+
+in craneLib.devShell {
+
+  # Inherit inputs from checks.
+  #checks = self.checks.${system}; TODO
+
+  # Additional dev-shell environment variables can be set directly
+  # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+  # Extra inputs can be added here; cargo and rustc are provided by default.
+  packages = with pkgs;
+    [
+      cargo-limit
+      cargo-nextest
+      just
+      bacon
+      cargo-dist
+      oranda
+      #treefmt-bin TODO
+      perSystem.cargo-nextest-xdg.default
+    ];
+    #++ treefmt-programs; # TODO
+}
---
+++ packages/llm-coverage.nix
@@ -0,0 +1,55 @@
+{inputs, pkgs, system, ...}: let
+  inherit (pkgs) lib;
+  craneLib = inputs.crane.mkLib pkgs;
+  src = craneLib.cleanCargoSource ../.;
+
+  craneLibLLvmTools =
+    craneLib.overrideToolchain
+    (inputs.fenix.packages.${system}.complete.withComponents [
+      "cargo"
+      "llvm-tools"
+      "rustc"
+    ]);
+
+    
+  # Common arguments can be set here to avoid repeating them later
+  commonArgs = {
+    inherit src;
+    strictDeps = true;
+
+    buildInputs =
+      [
+        # Add additional build inputs here
+        pkgs.nixos-rebuild
+        pkgs.age
+        pkgs.ssh-to-age
+      ]
+      ++ lib.optionals pkgs.stdenv.isDarwin [
+        # Additional darwin specific inputs can be set here
+        pkgs.libiconv
+      ];
+
+    # Additional environment variables can be set directly
+    # MY_CUSTOM_VAR = "some value";
+  };
+
+  # Build *just* the cargo dependencies, so we can reuse
+  # all of that work (e.g. via cachix) when running in CI
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+    
+in
+craneLibLLvmTools.cargoLlvmCov (commonArgs
+  // {
+    inherit cargoArtifacts;
+})
+
+
+# TODO: how do we constrain this package to linux only without constraing the main package?
+#lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+  #my-crate-llvm-coverage =
+  #craneLibLLvmTools.cargoLlvmCov (commonArgs
+    #// {
+      #inherit cargoArtifacts;
+    #})
+#};
